74c65ad7734d3bcbcc52ba02ab2c9694460bf4d6
Kevin <johnthanselvadurai@gmail.com>
2022-02-15 21:01:15 -0500

    Create honer.c

---
+++ Recursion/honer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum(int n){
+  
+  if(n==0)
+    return 0;
+  else
+   return sum(n-1)+n;	  
+
+}
+
+int main(){
+ 
+ int r;
+ r=sum(5); 
+ printf("%d ",r);	
+
+ return 0;
+}
