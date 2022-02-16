89ca40375251aa183f8b714d947baf5326ee60dc
Kevin <johnthanselvadurai@gmail.com>
2022-02-15 19:58:24 -0500

    Add files via upload

---
+++ Recursion/henoi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void TOH(int n,int a, int b, int c )
+{
+   if(n>0)
+    {
+       TOH(n-1,a,c,b);
+       printf("(%d,%d) \n",a,c);
+       TOH(n-1,b,a,c);
+    } 	   
+
+}
+
+
+int main()
+{
+  TOH(3,1,2,3);
+  return 0;
+
+
+}
