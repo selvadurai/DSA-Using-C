354e4b4f4b646ed26049fc24f4811766c44dc860
Kevin <johnthanselvadurai@gmail.com>
2022-02-19 22:57:52 -0500

    Add files via upload

---
+++ Matrices/TriangularMatrix.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Matrix
+{
+   int *A;
+   int n;
+
+};
+
+void Set(struct Matrix *m,int i,int j,int x){
+
+    if(i>=j)
+       m->A[i*(i-1)/2+j-1]=x;
+
+}
+
+int Get(struct Matrix m,int i,int j){
+    if(i>=j)
+      return m.A[i*(i-1)/2+j-1];
+     else
+       return 0; 
+}
+
+void Display(struct Matrix m){
+    
+     int i,j;
+
+     for(i=1;i<m.n;i++)
+     {
+
+        for(j=1;j<m.n;j++)
+        {
+           if(i>=j)
+             printf("%d ",m.A[i*(i-1)/2+j-1]);
+           else
+             printf("0 "); 
+        }
+        printf("\n");
+     
+     }
+
+}
+
+
+int main()
+{
+  struct Matrix m;
+  int i,j,x;
+
+  printf("Enter Dimension \n");
+
+  scanf("%d",&m.n);
+  m.A=(int *)malloc(m.n*(m.n+1)/2*sizeof(int));
+
+  printf("Enter all elements\n");
+  for(i=1;i<m.n;i++)
+  {
+     for(j=1;j<m.n;j++){
+         scanf("%d",&x);
+         Set(&m,i,j,x);
+     }     
+
+
+  }
+  
+  printf("\n\n");
+  Display(m);
+
+}
